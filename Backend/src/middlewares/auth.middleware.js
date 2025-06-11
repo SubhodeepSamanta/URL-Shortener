8db59031a8f46655b4f9d150bdf4832338d409c4
SubhodeepSamanta <subhodeepsamanta2005@gmail.com>
2025-06-11 21:25:59 +0530

    add dao changes

--- Backend/src/middlewares/auth.middleware.js
+++ Backend/src/middlewares/auth.middleware.js
@@ -1,5 +1,4 @@
 import { findUserById } from "../dao/user.js";
-import User from "../models/user.model.js";
 import { verifyToken } from "../utils/helper.js";
 
 export const authMiddleware = async (req, res, next) => {
@@ -7,7 +6,7 @@ export const authMiddleware = async (req, res, next) => {
         const token = req.cookies.accessToken;
         if (!token) return res.status(401).json({ message: "No token provided" });
         const decoded = await verifyToken(token);
-        const user = await User.findById(decoded.id);
+        const user = await findUserById(decoded.id);
         if (!user) return res.status(401).json({ message: "Unauthorised" });
         req.user = user;
         next();
